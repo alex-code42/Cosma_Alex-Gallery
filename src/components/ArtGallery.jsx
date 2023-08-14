3d5af4899d2841675df3cb1201c60aeb334d8b0c
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-14 12:19:59 +0200

    added fetching

---
+++ src/components/ArtGallery.jsx
@@ -0,0 +1,13 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const artURL = "https://example-apis.vercel.app/api/art";
+
+export default function ArtGallery() {
+  const { data, error, isLoading } = useSWR(artURL, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return <div>{data}</div>;
+}
