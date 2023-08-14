9dbfcd7c3234fdf5f57b2306c6cc21ec1fe30081
Alex <areichel1986@gmail.com>
2023-08-14 13:07:15 +0200

    yes

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3aca7235735af47f"; }
+/******/ 	__webpack_require__.h = function() { return "00e5f6c8758a0167"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
