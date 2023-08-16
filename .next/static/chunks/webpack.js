b6c7bfaa12a45457594cddbcb73b337aa3a15131
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-16 10:05:07 +0200

    installed localstorage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1cc7f73b2a3753d4"; }
+/******/ 		__webpack_require__.h = function() { return "2b30480565d588a1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
